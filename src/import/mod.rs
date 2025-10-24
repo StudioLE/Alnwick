cba1c36e65c8ea578bd3c10b001cfe279b4e9d73
Laurence Elsdon <git@elsdon.me>
2025-10-24 10:38:47 +0100

    chore: remove specific

--- src/import/mod.rs
+++
@@ -1,8 +0,0 @@
-mod import;
-mod shotsy;
-
-pub(crate) use import::*;
-#[cfg(test)]
-pub(crate) use shotsy::*;
-#[cfg(not(test))]
-pub(super) use shotsy::*;
--- src/statistics/mod.rs
+++
@@ -1,7 +0,0 @@
-mod bmi;
-mod simple_moving_average;
-mod trend;
-mod weight;
-
-pub use bmi::*;
-pub use weight::*;
