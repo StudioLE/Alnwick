1332065e7d5af8d30290b8d5794927f8adfbd02e
Laurence Elsdon <git@elsdon.me>
2025-10-24 15:08:18 +0100

    chore: switch to fullstack

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,13 +3,8 @@ pub(crate) use crate::contexts::page::context::PageContext;
 pub(crate) use crate::contexts::page::info::PageInfo;
 pub(crate) use crate::contexts::page::selector::PageSelector;
 pub(crate) use crate::contexts::settings::context::SettingsContext;
-pub(crate) use chrono::{Duration, NaiveDate, NaiveTime};
 pub(crate) use dioxus::logger::tracing::{debug, warn};
 pub(crate) use dioxus::prelude::*;
-#[cfg(test)]
-pub(crate) use expect::Expect;
-pub(crate) use regex::Regex;
-pub(crate) use serde::{Deserialize, Serialize};
 pub(crate) use std::collections::BTreeMap;
 pub(crate) use std::error::Error;
 pub(crate) use std::fmt::{Display, Formatter};
