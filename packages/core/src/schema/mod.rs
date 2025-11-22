f3cb25172148c74e48a17b1301db35f560d675de
Laurence Elsdon <git@elsdon.me>
2025-11-22 09:58:01 +0000

    style: Apply code style with clippy

--- packages/core/src/schema/mod.rs
+++ packages/core/src/schema/mod.rs
@@ -1,13 +1,13 @@
 pub use episode::EpisodeInfo;
 pub use episode_kind::*;
+pub use podcast::PodcastInfo;
 pub use podcast_category::*;
 pub use podcast_feed::*;
-pub use podcast::PodcastInfo;
 pub use podcast_kind::*;
 
 pub mod episode;
 mod episode_kind;
+pub mod podcast;
 mod podcast_category;
 mod podcast_feed;
-pub mod podcast;
 mod podcast_kind;
